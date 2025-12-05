e5ff0d0f56003cde2b642bc444d0c038686e577d
Gianluca Zarrelli <g.zarrelli96@gmail.com>
2025-12-05 00:41:22 -0300

    asa

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <footer>
-      <p>@2025 Pagina creada por Daniela Tarifa</p>
+      <p>@2025 Pagina creada por Gianluca Zarrelli</p>
     </footer>
   );
 };
